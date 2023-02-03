59f9eea49e137aec663a82e8cf81974fbc531579
Anjali Prakash <anjali.prakash1501@gmail.com>
2023-02-03 21:32:13 +0530

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link, useParams } from 'react-router-dom';
 
 function RenderDish({dish}) {
+	//if(dish!=null){
         return(
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="/100%" src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
+        	<div className="col-12 col-md-5 m-1">
+          		<Card>
+            		<CardImg width="/100%" src={dish.image} alt={dish.name} />
+            		<CardBody>
+              			<CardTitle>{dish.name}</CardTitle>
+              			<CardText>{dish.description}</CardText>
+            		</CardBody>
+          		</Card>
+        	</div>
         );
+    ///}
+    
 }
 
 function RenderComments({comments}){
@@ -28,8 +32,7 @@ function RenderComments({comments}){
     		       	           <p>-- {comment.author} , {new Intl.DateTimeFormat('en-Us', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
     		       	        </li>
     		       	    );
-    		       	}
-    		       	)}
+    		       	})}
     		       	</ul>
     		    </div>
     		);
@@ -37,16 +40,36 @@ function RenderComments({comments}){
     	else{
     		return(<div></div>);
     	}
-}
+ }
 	const DishDetail = (props)=> {
-	    if (props.dish !=null) {
+		let {dishId}=useParams();
+		console.log(dishId)
+		const dish=props.dishes.filter((dish)=>dish.id==dishId)[0]
+		const comment=props.comments.filter((comment)=>comment.dishId==dishId)
+		console.log(dish)
+		
+	    if (dish !=null) {
 			return (
 				<div className = "container">
+				 	<div className="row">
+              			<Breadcrumb>
+                   			
+                   			<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                   			<BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+              			</Breadcrumb>
+              			<div className="col-12">
+                  			<h3>{dish.name}</h3>
+                  			<hr />
+              			</div>
+          			</div>
 				    <div className = "row">
 				       
-				            <RenderDish dish = {props.dish} />
-				  
-				            <RenderComments comments = {props.dish.comments} />
+                        	<RenderDish dish={dish} />
+                        
+                        
+                            <RenderComments comments={comment} />
+                        
+				            
 				    </div>    
 			    </div>
 			);
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
@@ -9,7 +9,7 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useParams } from 'react-router-dom';
 
 class Main extends Component{
   constructor(props) {
@@ -31,19 +31,28 @@ class Main extends Component{
       return(
         <Home
             dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
             leader={this.state.leaders.filter((leader) => leader.featured)[0]}
 
         />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail 
+                dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          />
+      );
+    };
     return (
       <div>
         <Header />
         <Routes>
-          <Route path="/home" element={<HomePage/>} />
-          <Route path="/menu" element={<Menu dishes={this.state.dishes} /> }/>
-          <Route path="/contactus" element={<Contact />} />
+          <Route exact path='/home' element={<HomePage/>} />
+          <Route exact path='/menu' element={<Menu dishes={this.state.dishes} /> }/>
+          <Route path='/menu/:dishId' element={<DishDetail dishes ={this.state.dishes} comments={this.state.comments}/>} />
+          <Route exact path='/contactus' element={<Contact/>} />
           <Route path="*" element = {<Navigate to ="/home" />} />
         </Routes>
         
